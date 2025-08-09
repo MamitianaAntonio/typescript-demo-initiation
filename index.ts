a72fad4f608d91174838265493b8534b721d58e7
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-08-09 22:45:53 +0000

    test : add manual test using interface

--- index.ts
+++ index.ts
@@ -1,4 +1,47 @@
-let typescriptVariable: number | string = "hello";
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  available: boolean;
+}
 
-typescriptVariable = 2;
-console.log(typescriptVariable);
+class Library {
+  private books: Book[] = [];
+
+  addBook(book: Book): void {
+    this.books.push(book);
+  }
+
+  listBook(): void {
+    this.books.forEach((b) => {
+      console.log(b);
+    });
+  }
+
+  searchBookByTitle(title: string): Book | undefined {
+    for (let b of this.books) {
+      if (b.title === title) {
+        return b;
+      }
+    }
+    return undefined;
+  }
+}
+
+// manual test using object
+const library = new Library();
+library.addBook({
+  id: 1,
+  title: "Typescript",
+  author: "Antonio",
+  available: true,
+});
+library.addBook({
+  id: 2,
+  title: "Typescript 2",
+  author: "Another",
+  available: false,
+});
+
+library.listBook();
+console.log(library.searchBookByTitle("Typescript"));
